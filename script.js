6d69bdb40aee3686a8e8ecae5fb38f9332dab41c
emilvn <emilvnielsen@hotmail.com>
2023-04-27 14:40:11 +0200

    emil 1

--- script.js
+++ script.js
@@ -3,18 +3,5 @@
 window.addEventListener("load", start);
 
 function start() {
-  console.log("Hello Emil2");
-  console.log("Hello Daniel");
-  console.log("Hello EMil NEJ");
-  console.log("HEJ");
-  console.log("konfliktsky");
-  console.log("nej");
-  console.log("jo");
-  console.log("Nu er vi i en konflikt - du er dum!");
-  console.log(" piksvin - du er dum!");
-
-  console.log("intet at se her");
-
-  console.log("look away");
-  console.log("Nu afprøver jeg merge conflict");
+  console.log("emil 1")
 }
